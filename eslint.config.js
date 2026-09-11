c0a78525053b8dc61d3b06fa0f179839afb1b58d
Alexander Sokolov <alex@MacBook-Air-Alexander.local>
2026-09-11 13:36:48 +0300

    rename eslint config and change version

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   stylistic.configs.recommended,
