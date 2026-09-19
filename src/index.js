1fe8096cde0767ecd8393d35aa4af494761f7983
Alexander Sokolov <alexsokolov123654@mail.ru>
2026-09-20 00:08:25 +0300

    add i18next logic, fix functions at View.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,93 @@
-import { string } from 'yup';
+import { string, setLocale } from 'yup';
 import { proxy, subscribe } from 'valtio/vanilla'
 import { updateUI } from './View.js';
+import i18next from 'i18next';
+import resources from './locales/index.js';
 
-const searchSchema = string()
-  .required('Не должно быть пустым')
-  .url('Ссылка должна быть валидным URL')
+const newInstance = i18next.createInstance()
 
-const state = proxy({
-  searchForm: {
-    status: 'filling',  // processing, success, failed
-    errors: [],
-    links: []
-  }
-})
+newInstance.init({
+  lng: 'ru',
+  debug: false,
+  resources,
+}).then(() => {
 
-const form = document.querySelector('#search-form')
+  setLocale({
+    mixed: {
+      required: () => ({ key: 'empty' }),
+    },
+    string: {
+      url: () => ({ key: 'wrong' }),
+    },
+  })
 
-const validate = (url) => {
-  if (state.searchForm.links.includes(url)) {
-    state.searchForm.errors = ['Ошибка: адрес уже добавлен']
-    state.searchForm.status = 'failed'  // failed
-    // console.log(state.searchForm) //
-    return
-  }
-  searchSchema.validate(url)
-    .then(() => {
-      state.searchForm.links.push(url)
-      state.searchForm.status = 'success' // success
-      // console.log(state.searchForm) //
-      state.searchForm.errors = []
-    })
-    .catch(err => {
+  // entered string is required and must be url
+  const searchSchema = string()
+    .required()
+    .url()
+
+  const state = proxy({
+    searchForm: {
+      status: 'filling',  // processing, success, failed
+      errors: [],
+      links: []
+    }
+  })
+
+  const form = document.querySelector('#search-form')
+
+  // setting localization
+  document.querySelector('h1').textContent = newInstance.t('header.h1')
+  document.querySelector('#quote').textContent = newInstance.t('header.quote')
+  document.querySelector('#headerLabel').textContent = newInstance.t('header.label')
+  document.querySelector('#rss-search').placeholder = newInstance.t('header.placeholder')
+  document.querySelector('#submit-button').textContent = newInstance.t('header.submitButton')
+  document.querySelector('#rss-example').textContent = newInstance.t('header.example')
+
+  // validate entered URL
+  const validate = (url) => {
+    if (state.searchForm.links.includes(url)) {
+      state.searchForm.errors = [newInstance.t('header.messages.exists')]
       state.searchForm.status = 'failed'  // failed
-      // console.log(state.searchForm) //
-      state.searchForm.errors = err.errors
-    })
-}
-
-subscribe(state, () => updateUI(state.searchForm))
-
-form.addEventListener('input', (e) => {
-  const formData = new FormData(form)
-  const url = formData.get('search')
-  if (url !== '') {
-    state.searchForm.status = 'filling'  // filling
-    // console.log(state.searchForm) //
+      console.log(state.searchForm) //
+      return
+    }
+    searchSchema.validate(url)
+      .then(() => {
+        state.searchForm.links.push(url)
+        state.searchForm.status = 'success' // success
+        console.log(state.searchForm) //
+        state.searchForm.errors = []
+      })
+      .catch(err => {
+        state.searchForm.status = 'failed'  // failed
+        console.log(state.searchForm) //
+        state.searchForm.errors = err.errors.map(({ key }) => newInstance.t(`header.messages.${key}`))
+      })
   }
-})
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const formData = new FormData(form)
-  const url = formData.get('search')
-  state.searchForm.status = 'processing'  // processing
-  // console.log(state.searchForm) //
-  validate(url)
+  // subscribe View.js on change of state
+  subscribe(state, () => updateUI(state.searchForm, newInstance))
+
+  // when user inputs address
+  form.addEventListener('input', (e) => {
+    const formData = new FormData(form)
+    const url = formData.get('search')
+    if (url !== '') {
+      state.searchForm.status = 'filling'  // filling
+      console.log(state.searchForm) //
+    }
+  })
+
+  // when "Add" button is pressed
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const url = formData.get('search')
+    state.searchForm.status = 'processing'  // processing
+    console.log(state.searchForm) //
+    validate(url)
+  })
+}).catch((err) => {
+  console.error('Ошибка инициализации i18next:', err)
 })
