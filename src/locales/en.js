4e5591a7598b9c98e78ad46edcb4f1c688e94ee2
Alexander Sokolov <alexsokolov123654@mail.ru>
2026-09-20 00:07:00 +0300

    add rus, eng locales

---
+++ src/locales/en.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    header: {
+      h1: 'RSS aggregator',
+      quote: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+      label: 'RSS link',
+      placeholder: 'RSS link',
+      submitButton: 'Add',
+      example: 'For example: https://ru.wikipedia.org/wiki/Lorem_ipsum',
+      messages: {
+        empty: 'Must not be empty',
+        wrong: 'The link must be a valid URL',
+        exists: 'Address has already been added',
+        correct: 'RSS uploaded successfully',
+      }
+    }
+  }
+}
